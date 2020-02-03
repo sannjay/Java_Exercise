2acdc098af1a177eb66de8d9d404dbc33237fb30
sannjay <sanrathore001@gmail.com>
2020-02-03 09:08:41 +0530

    Adding Rectangle Program with printf

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math.*;
+public class Rectangle {
+  public static void main(String[] args) 
+  {
+  double a=5.6,b=8.5;
+ 
+  System.out.printf("Area is %.1f + %.1f = %.2f \n",a,b,a*b);
+
+  System.out.printf("Perimeter is 2 * ("+a+" + "+b+") = %.2f", 2*(a+b));
+  
+ }
+}

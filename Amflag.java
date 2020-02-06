94b32addb105a0c9724a3e4e7c8c4c32df78d00b
sannjay <sanrathore001@gmail.com>
2020-02-06 23:14:32 +0530

    Adding America flag

---
+++ Amflag.java
@@ -0,0 +1,26 @@
+public class Amflag {
+  public static void main(String[] args) 
+  {
+     for(int r=0; r<15; r++)
+     {
+      System.out.println(); 
+       for(int i=0; i<46; i++)
+       {
+         if(r<9 && i<6)
+         {
+          if (r%2==0)
+          System.out.print("* ");
+         else
+         if(i<5)
+          System.out.print(" *");
+         else System.out.print("  ");
+         }
+        else
+         if(r<9 && i>39) 
+         System.out.print("");
+        else
+         System.out.print("=");
+      }
+    }
+  }
+}
